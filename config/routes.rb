683ef3ed0ed4bf17e3dfa2d56f3e9c244b04154b
UniqueNL <samtaal@outlook.com>
2016-10-12 18:32:45 +0200

    Constructed controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "products#index"
+  root "pages#home"
 
   resources :categories
   resources :products
-  resources :profiles
+  resources :profiles, only: [:new, :edit, :create, :update]
 end
