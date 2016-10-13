e8a026dd98ba191f3c0ede228c0d9dde28e90fd3
Tjitske Lohstroh <tjitske@lohstroh.nl>
2016-10-13 16:10:16 +0200

    add opzet product/category controllers en view

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,9 @@
 class Category < ApplicationRecord
   has_many :products
+
+  def self.order_by_name
+    order(:name)
+  end
+
 end
+
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,18 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+
+  def self.order_by_name
+    order(:name)
+  end
+
+  # def self.order_by_date
+  #   order(:date)
+  # end
+
+  def self.order_by_category
+    joins(:category).order('categories.name')
+  end
+
+
 end
