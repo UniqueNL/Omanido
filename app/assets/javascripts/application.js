7a076de4edb25fb65252105c58ed1660b693d51e
UniqueNL <samtaal@outlook.com>
2016-10-18 17:29:28 +0200

    added sort and styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,3 +9,12 @@ $(function() {
     $('.notice-alert').slideUp(500);
   }, 2000);
 });
+
+function sort(type) {
+  if ($(type).hasClass("selected")) {
+    $(type).innerHTML("Hi");
+  }
+  else {
+    $(type).addClass("selected");
+  }
+}
